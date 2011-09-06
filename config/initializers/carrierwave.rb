c27bcc0099978409ba9f8e08373b1ef85bfd1f12
Trevor Turk <trevorturk@gmail.com>
2011-09-05 22:18:23 -0500

    max s3 keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,8 @@
-# CarrierWave.configure do |config|
-#   config.fog_credentials = {
-#     :provider               => 'AWS',       # required
-#     :aws_access_key_id      => 'xxx',       # required
-#     :aws_secret_access_key  => 'yyy',       # required
-#     :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
-#   }
-#   config.fog_directory  = 'name_of_directory'                     # required
-# end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAJNPIOUJBLXWB5NNA',
+    :aws_secret_access_key  => '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'
+  }
+  config.fog_directory  = 'portwist'
+end

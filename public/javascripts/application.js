af20bdb2c3f53ae03fa5f7c5cdb05332373121b5
Trevor Turk <trevorturk@gmail.com>
2011-12-03 16:57:27 -0600

    fancy file upload buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
 
-  // 
-  $('.photo_set_photo_file_field').hover(function() {
-   $(this).css('cursor','pointer');
-   }, function() {
-   $(this).css('cursor','auto');
+  //
+  // file uploads
+  //
+
+  var SITE = SITE || {};
+
+  SITE.fileInputs = function() {
+   var $this = $(this),
+       $val = $this.val(),
+       valArray = $val.split('\\'),
+       newVal = valArray[valArray.length-1],
+       $button = $this.siblings('.button'),
+       $fakeFile = $this.siblings('.file-holder');
+   if(newVal !== '') {
+     $button.text('Photo Chosen');
+     if($fakeFile.length === 0) {
+       $button.after('<span class="file-holder">' + newVal + '</span>');
+     } else {
+       $fakeFile.text(newVal);
+     }
+   }
+  };
+
+  $(document).ready(function() {
+   $('.file-wrapper input[type=file]').bind('change focus click', SITE.fileInputs);
   });
-  
-  $(".photo_set_photo_file_field").change(function(){
-    $(this).parent().addClass("photo_set_photo_file_field_chosen");
-   });
 
 });

a789823503c2418a1f09adbd490c52176b67dbd8
Max Mearsheimer <mmearshe@gmail.com>
2012-02-12 14:00:37 -0600

    more size messing

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -10,7 +10,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :home do
-    process :resize_to_limit => [273, 232]
+    process :resize_to_limit => [276, 235]
   end
 
   version :m do

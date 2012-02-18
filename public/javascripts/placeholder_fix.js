7346df9664cc3e669c51b238589c676caba66a8c
Trevor Turk <trevorturk@gmail.com>
2012-02-17 18:43:04 -0600

    install the placeholder fix

--- public/javascripts/placeholder_fix.js
+++ public/javascripts/placeholder_fix.js
@@ -1,6 +1,3 @@
-// Released under MIT license: http://www.opensource.org/licenses/mit-license.php
-
-// good javascript
 (function ($) {
     'use strict';
     $(function () {
@@ -29,5 +26,3 @@ input.val('');
     });
 })(jQuery);
 
-// minified javascript
-(function(j,p){j(function(){j("["+p+"]").focus(function(){var i=j(this);if(i.val()===i.attr(p)){i.val("").removeClass(p)}}).blur(function(){var i=j(this);if(i.val()===""||i.val()===i.attr(p)){i.addClass(p).val(c.attr(p))}}).blur().parents("form").submit(function(){j(this).find(p).each(function(){var i=j(this);if(i.val()===i.attr(p)){i.val("")}})})})})(jQuery,"placeholder");

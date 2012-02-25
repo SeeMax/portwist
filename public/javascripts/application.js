6257fe58af1bac83f427b2e30d07c4d5fcde16d3
Trevor Turk <trevorturk@gmail.com>
2012-02-24 23:08:25 -0600

    fancy uploader for you, jerk!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,40 +1,11 @@
 $(document).ready(function(){
 
-  // modals
-
   $(".fancybox").fancybox();
-
-  //
-  // file uploads
-  //
-
-  var SITE = SITE || {};
-
-  SITE.fileInputs = function() {
-   var $this = $(this),
-       $val = $this.val(),
-       valArray = $val.split('\\'),
-       newVal = valArray[valArray.length-1],
-       $button = $this.siblings('.button'),
-       $fakeFile = $this.siblings('.file-holder');
-   if(newVal !== '') {
-     $button.text('GOT IT');
-     if($fakeFile.length === 0) {
-       $button.after('<span class="file-holder">' + newVal + '</span>');
-     } else {
-       $fakeFile.text(newVal);
-     }
-   }
-  };
-
-  $(document).ready(function() {
-   $('.file-wrapper input[type=file]').bind('change focus click', SITE.fileInputs);
-  });
 
   $('#photo_set_your_name').focus(function(){
     $(this).val(' ');
   });
-  
+
   $('#photo_set_your_title').focus(function(){
     $(this).val(' ');
   });
--- public/javascripts/uploader.js
+++ public/javascripts/uploader.js
@@ -1,10 +1,9 @@
-function readURL(input, image) {
+function readURL(input, button) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
 
     reader.onload = function (e) {
-      image.attr('src', e.target.result);
-      image.show();
+      button.css('background-image', "url('" + e.target.result + "')");
     }
 
     reader.readAsDataURL(input.files[0]);
